b48ad83639fe49b03d8d1213524ed34c1d59b44e
Kimberly <kyz@andrew.cmu.edu>
2019-08-05 23:22:30 -0400

    init

---
+++ calculate.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scrape import *
+
+op_cash, capex= collect_fcf_data(cash_url, browser)
+fcf= op_cash-capex
+revenue, ebitda= collect_terminal_val_data(income_url, browser)
+ebitda_margin= ebitda/revenue
+stats= get_financial_stats(stats_url, browser)
+interest= get_interest_expense(interest_url, browser)
+
+def get_growth_rates(ebitda_margin):
+	growth_rates= []
+	for i in range(1, len(ebitda_margin)):
+		diff= (ebitda_margin[i] - ebitda_margin[i-1])
+		growth_rates.append(diff/ebitda_margin[i-1])
+	return np.array(growth_rates)
+
+def get_wacc(e, d, tax_rate, beta, krf, rp, interest):
+	equity_portion= e/(d+e)
+	debt_portion= d/(d+e)
+	equity_cost= beta*rp + krf 
+	debt_cost= (interest/d)*100*(1-tax_rate)
+	return equity_cost*equity_portion + debt_cost*debt_portion
+
+def caculate_terminal_val(g, last_fcf, wacc):
+	return last_fcf*(1+g)/(wacc-g)
+
+g= np.mean(get_growth_rates(ebitda_margin))
+print(g)
+
+
---
+++ scrape.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+from re import sub
+from decimal import Decimal
+import numpy as np 
+
+cash_url='https://www.macrotrends.net/stocks/charts/AAPL/apple/cash-flow-statement'
+income_url= 'https://www.macrotrends.net/stocks/charts/AAPL/apple/income-statement?freq=A'
+stats_url= "https://finance.yahoo.com/quote/AAPL/key-statistics?p=AAPL"
+
+interest_url="https://finance.yahoo.com/quote/AAPL/financials?p=AAPL"
+
+options = Options()
+options.add_argument("--headless")
+browser= webdriver.Chrome(options=options)
+
+def currency_to_num(currency):
+	return Decimal(sub(r'[^\d.]', '', currency))
+
+def billion_to_e(num):
+	return Decimal(sub(r'B', 'e9', num))
+
+def collect_fcf_data(url, browser):
+	browser.get(url)
+	soup= BeautifulSoup(browser.page_source, 'lxml')
+	op_cash_row= soup.select('#row9jqxgrid > div:nth-child(n+3) > div')
+	capex_row= soup.select('#row10jqxgrid > div:nth-child(n+3) > div')
+	op_cash= [currency_to_num(i.get_text() + 'e6') for i in op_cash_row]
+	capex= [currency_to_num(i.get_text()+'e6') for i in capex_row]
+	return np.array(op_cash), np.array(capex)
+
+def collect_terminal_val_data(url, browser):
+	browser.get(url)
+	soup= BeautifulSoup(browser.page_source, 'lxml')
+	revenue_row= soup.select('#row0jqxgrid > div:nth-child(n+3) > div')
+	ebitda_row= soup.select('#row16jqxgrid > div:nth-child(n+3) > div')
+	revenue= [currency_to_num(i.get_text() + 'e6') for i in revenue_row]
+	ebitda= [currency_to_num(i.get_text() + 'e6') for i in ebitda_row]
+	return np.array(revenue), np.array(ebitda)
+
+def get_financial_stats(url, browser):
+	browser.get(url)
+	soup= BeautifulSoup(browser.page_source, 'lxml')
+	targets= ["Market Cap (intraday)", "PEG Ratio (5 yr expected)", "Enterprise Value/EBITDA", "Total Debt (mrq)", "Beta (3Y Monthly)", "50-Day Moving Average", "200-Day Moving Average"]
+	res= {}
+	stored= soup.find_all('td')
+	for i, val in enumerate(stored):
+		if len(res.keys()) == len(targets):
+			break
+		text= val.find("span")
+		if text and text.get_text() in targets:
+			res[text.get_text()]= billion_to_e(stored[i+1].get_text())
+	return res
+
+def get_interest_expense(url, browser):
+	browser.get(url)
+	soup= BeautifulSoup(browser.page_source, 'lxml')
+	stored= soup.find_all('td')
+	res= {}
+	for i, val in enumerate(stored):
+		text= val.get_text()
+		if text == 'Interest Expense':
+			res[text]= currency_to_num(stored[i+1].get_text() + 'e3')
+			break
+	return res
+
+
+
+
